850301ce0dbf3c76972a0542f38bba36db79cc4b
CANSU GÜREL <cansugurel@eskisehir.edu.tr>
2023-04-26 23:59:46 +0300

    first commit fro react front

---
+++ src/models/User.js
@@ -0,0 +1,8 @@
+class User {
+    constructor(full_name, email, password, role) {
+      this.full_name = full_name
+      this.email = email
+      this.password = password
+      this.role = role
+    }
+  }
---
+++ src/models/UserRating.js
@@ -0,0 +1,8 @@
+class UserRating {
+    constructor(user, service, rating, rated_at) {
+      this.user = user
+      this.service = service
+      this.rating = rating
+      this.rated_at= rated_at
+    }
+  }
