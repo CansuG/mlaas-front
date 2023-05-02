019096af5cfebf1ae0f7ef8b5d97786133a2281c
Şevval Türkyılmaz <sevvalturkyilmazz@gmail.com>
2023-05-02 04:43:15 +0300

    front- home

--- src/App.js
+++ src/App.js
@@ -3,12 +3,17 @@ import React from 'react';
 import Login from './pages/login/login.jsx';
 import TopBar from "./components/topbar/TopBar";
 import Header from "./components/header/Header";
+import Footer from './components/footer/Footer';
+import Models from "./components/models/Models"
 function App() {
   return (
     <div className="App">
      <div className='gradient__bg'>
        <TopBar/>
        <Header/>
+       <div>
+         <Models/>
+       <Footer/></div>
      </div>
     </div>
   );
